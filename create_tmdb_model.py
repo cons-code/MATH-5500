610364c945286f9dfe5b36459d15d42a611c6384
zack-holden <zachary.holden@jacobs.com>
2024-10-24 12:27:33 -0400

    add third cut features and covariate interactions

--- create_tmdb_model.py
+++ create_tmdb_model.py
@@ -19,7 +19,7 @@ def parse_inputs():
 
     # Add file parameters.
     parser.add_argument('--fpath', type=str, default='./data', help='Relative filepath to input directory.')
-    parser.add_argument('--fname', type=str, default='first_cut_adj.csv', help='Name of input CSV file containing TMDB data.')
+    parser.add_argument('--fname', type=str, default='final_cut.csv', help='Name of input CSV file containing TMDB data.')
     parser.add_argument('--n', type=int, default=500, help='Number of movies in TMDB data.')
 
     # Parse input arguments.
@@ -47,17 +47,22 @@ def main():
     cdf = df.get_clean_pd_df()
 
     # Generate pair plot of continuous covariates.
-    sns.pairplot(cdf[['opening_revenue', 'budget', 'num_production_companies', 'runtime', 'popularity']])
+    sns.pairplot(cdf[['opening_revenue', 'budget', 'num_production_companies', 'runtime', 'month', 'pct_indie']])
 
     # Generate OLS model.
     Y = ['opening_revenue']
-    X = ['budget', 'popularity', 'runtime', 'is_series', 'is_fall', 'is_holiday']
+    X = ['budget', 'runtime', 'writers_room_size', 'num_production_companies', \
+         'pct_indie', 'is_indie', \
+         'cast_xp_sum', 'director_xp', 'director_rating_median', \
+         'prev_film_release', 'prev_film_rating', \
+         'is_feb', 'is_mar', 'is_oct', 'is_dec', \
+         'is_drama', 'is_horror', 'is_comedy', 'is_western', 'is_crime', 'is_documentary', 'is_sci_fi', 'is_music', 'is_family']
 
-    cov_transform = {'opening_revenue': 'sqrt', 'runtime': 'sq'}
+    cov_transform = {'opening_revenue': 'sqrt'}
     # cov_transform = None
     
-    # cov_joint = [['is_series', 'is_holiday']]
-    cov_joint = None
+    cov_joint = [['prev_film_release', 'prev_film_rating'], ['is_horror', 'is_oct'], ['cast_xp_sum', 'director_xp']]
+    # cov_joint = None
 
     _, res = mdl.get_ols_model(cdf, Y, X, cov_transform=cov_transform, cov_joint=cov_joint)
 
